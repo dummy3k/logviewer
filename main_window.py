1d29fdd6db64a5eb6fea2b17b5d9d6ec2870c2de
dummy3k <l4711@gmx.net>
2011-03-26 17:20:27 +0100

    mark all as read, unread items are bold

--- main_window.py
+++ main_window.py
@@ -13,14 +13,27 @@ log = logging.getLogger(__name__)
 MAX_LIST_ITEMS = 100
 
 class LoggerInfo():
-    def __init__(self, name, tree_node):
+    def __init__(self, name, tree_node, tree_control):
         self.name = name
         self.unread_count = 0
         self.tree_node = tree_node
         self.messages = []
+        self.tree = tree_control
 
     def __str__(self):
         return "LoggerInfo('%s', %s)" % (self.name, self.unread_count)
+
+    def Append(self, m):
+        self.messages.append(m)
+        self.unread_count += 1
+        self.tree.SetItemText(self.tree_node,
+            "%s (%s)" % (self.name, self.unread_count))
+        self.tree.SetItemBold(self.tree_node, True)
+
+    def MarkAsRead(self):
+        self.unread_count = 0
+        self.tree.SetItemText(self.tree_node, self.name)
+        self.tree.SetItemBold(self.tree_node, False)
 
 class LogLinesListCtrlPanel(wx.ListCtrl, listmix.ListCtrlAutoWidthMixin):
     def __init__(self, parent):
@@ -40,15 +53,15 @@ class MyFrame(wx.Frame):
 
         menuBar = wx.MenuBar()
         menu1 = wx.Menu()
-        menu1.Append(101, "&Mercury", "This the text in the Statusbar")
-        menuBar.Append(menu1, "&Planets")
+        menu1.Append(101, "&Mark all as read")
+        menuBar.Append(menu1, "&Messages")
+        self.Bind(wx.EVT_MENU, self.MenuMarkAllAsRead)
         self.SetMenuBar(menuBar)
 
         filename = '/tmp/logcat.log'
         self.tree = wx.TreeCtrl(self.splitter, wx.ID_ANY, wx.DefaultPosition,
                            wx.DefaultSize, wx.TR_DEFAULT_STYLE)
         self.root = self.tree.AddRoot(os.path.basename(filename))
-        #~ self.tree.Bind(wx.EVT_LEFT_DCLICK, self.TreeOnLeftDClick)
         self.Bind(wx.EVT_TREE_SEL_CHANGED, self.TreeOnSelChanged, self.tree)
 
         self.list_view = LogLinesListCtrlPanel(self.splitter)
@@ -56,13 +69,10 @@ class MyFrame(wx.Frame):
         self.list_view.InsertColumn(1, "logger_name")
         self.list_view.InsertColumn(2, "thread_id", wx.LIST_FORMAT_RIGHT)
         self.list_view.InsertColumn(3, "message")
-        #~ self.list_view.Append(('level', 'logger_name', 'thread_id', 'message'))
 
         self.splitter.SetMinimumPaneSize(20)
         self.splitter.SplitVertically(self.tree, self.list_view, 240)
         self.Bind(wx.EVT_SIZE, self.OnSize)
-        self.Bind(wx.EVT_SPLITTER_SASH_POS_CHANGED, self.OnSashChanging)
-        self.Bind(wx.EVT_SPLITTER_SASH_POS_CHANGING, self.OnSashChanging)
 
         self.reader = FileReader(filename, self)
         self.Bind(EVT_LINE_READ, self.OnUpdate)
@@ -72,8 +82,9 @@ class MyFrame(wx.Frame):
         w,h = self.GetClientSizeTuple()
         self.splitter.SetDimensions(0, 0, w, h)
 
-    def OnSashChanging(self, event):
-        log.debug("sash changing to %s\n" % str(event.GetSashPosition()))
+    def MenuMarkAllAsRead(self, event):
+        for loginfo in self.logger_infos.values():
+            loginfo.MarkAsRead()
 
     def TreeOnSelChanged(self, event):
         log.debug("TreeOnLeftDClick()")
@@ -89,9 +100,7 @@ class MyFrame(wx.Frame):
                 except UnicodeDecodeError:
                     log.error("UnicodeDecodeError in TreeOnSelChanged()")
 
-            loginfo.unread_count = 0
-            #~ log.debug("%s.unread_count: %s" % (logger_name, loginfo.unread_count))
-            self.tree.SetItemText(loginfo.tree_node, loginfo.name)
+            loginfo.MarkAsRead()
 
         event.Skip()
 
@@ -107,18 +116,14 @@ class MyFrame(wx.Frame):
         if not logger_name in self.logger_infos:
             #~ log.debug("New logger_name: %s" % logger_name)
             child = self.tree.AppendItem(self.root, logger_name)
-            loginfo = LoggerInfo(logger_name, child)
+            loginfo = LoggerInfo(logger_name, child, self.tree)
             self.tree.SetPyData(child, loginfo)
             self.logger_infos[logger_name] = loginfo
             if len(self.logger_infos) == 1:
                 self.tree.Expand(self.root)
 
         loginfo = self.logger_infos[logger_name]
-        loginfo.unread_count += 1
-        #~ log.debug("%s.unread_count: %s" % (logger_name, loginfo.unread_count))
-        self.tree.SetItemText(loginfo.tree_node,
-            "%s (%s)" % (logger_name, loginfo.unread_count))
-        loginfo.messages.append((level, logger_name, thread_id, message))
+        loginfo.Append((level, logger_name, thread_id, message))
 
 def main():
     app = wx.App()
